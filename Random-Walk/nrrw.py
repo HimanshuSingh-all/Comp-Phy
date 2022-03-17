b0832f4976ef5f84bf3dfdb857371a7416baa993
Himanshu Singh <himanshusinghp1@gmail.com>
2022-03-17 17:56:10 +0530

    commit-2 refspec

---
+++ Random-Walk/nrrw.py
@@ -0,0 +1,12 @@
+#PG 82 ALGORITHM OF K BINDER AND  D.W.H. : MONTE CARLO SIMULATIONS IN PHYSICS
+#HIMANSHU SINGH
+
+#coordination number is 4
+import matplotlib.pyplot as plt
+import numpy as np
+
+init=(0,0)
+
+steps=[(1,0),(0,1),(-1,0),(0,-1)]
+
+N=100
