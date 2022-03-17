f985d6a54217da2e75f39defb36014caa29af361
Himanshu Singh <himanshusinghp1@gmail.com>
2022-03-17 18:45:15 +0530

    Plots added and saved

--- Physical-Pendulum/pend.py
+++ Physical-Pendulum/pend.py
@@ -1,12 +1,9 @@
 
-
-
-
 import numpy as np 
 import matplotlib.pyplot as plt
 
 ## vec is of the form [t x dx/dt]
-gamma=0.0
+gamma=0.1
 g=9.8
 def rhs(vec):
     return np.array( [1,vec[2], -gamma*vec[2]-g*np.sin(vec[1]) ] )
@@ -19,8 +16,6 @@ def mod_euler(init_vec:list, t_final, step):
 
         tmpv= soln[-1,:]+step*rhs(soln[-1,:]) 
         soln=np.vstack( [ soln, soln[-1,:]+step/2*(rhs(soln[-1,:]) + rhs(tmpv) )] )
-        ##soln[-1, :]
-        #print(soln[-1])
     return soln
 
 def euler_cromer(init_vec:list, t_final, step):
@@ -30,7 +25,6 @@ def euler_cromer(init_vec:list, t_final, step):
         soln=np.vstack([soln, soln[-1]])
         soln[-1,0]+=step
         for i in range(1,soln.shape[1]):
-            print(i)
             soln[-1,soln.shape[1]-i]+=rhs(soln[-1])[soln.shape[1]-i]*step
     return soln
 
@@ -39,6 +33,11 @@ t_final=10
 
 soln=mod_euler(init,t_final,0.1)
 soln1=euler_cromer(init,t_final,0.1)
-#plt.plot(soln[:,0],soln[:,1])
-plt.plot(soln1[:,0],soln1[:,1])
+plt.ylabel(r"$\theta$")
+plt.xlabel(r"Time")
+
+plt.plot(soln[:,0],soln[:,1],label="Euler-Method")
+plt.plot(soln1[:,0],soln1[:,1],linestyle="dashdot",label="Euler-Cromer Method")
+plt.legend()
+plt.savefig("Smooth Pendulum.png")
 plt.show()
